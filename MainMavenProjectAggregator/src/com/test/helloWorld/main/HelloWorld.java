ed18d748a73104eea04f0ed28c0af3d1ab8a20cb
Tudor <codrea_tudor@yahoo.com>
2016-11-07 15:00:59 +0200

    Fixes the inter-projects dependencies.

--- MainMavenProjectAggregator/src/com/test/helloWorld/main/HelloWorld.java
+++ MainMavenProjectAggregator/src/com/test/helloWorld/main/HelloWorld.java
@@ -1,14 +1,16 @@
 package com.test.helloWorld.main;
 
-import com.test.childProjects.connection.TestProject1Connection;
+import com.test.childProjects.connection.*;
+import com.test.util.Constants;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 
 		System.out.println("Hi Jenkins builder !!!");
-		System.out.println("Hi Jenkins builder !!! "+new TestProject1Connection().mUseTest1Test1HelloConstant);
-		
+		System.out.println("Hi Jenkins builder !!! " + Constants.TEST1_HELLO);
+		System.out.println("Hi Jenkins builder !!!");
+
 	}
 
 }
